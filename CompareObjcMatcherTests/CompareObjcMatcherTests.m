0395b0e8a1168411532a679d4fa0458a3efcf642
tokorom <tokorom@gmail.com>
2013-12-21 09:38:00 +0900

    First commit

---
+++ CompareObjcMatcherTests/CompareObjcMatcherTests.m
@@ -0,0 +1,34 @@
+//
+//  CompareObjcMatcherTests.m
+//  CompareObjcMatcherTests
+//
+//  Created by ytokoro on 12/21/13.
+//  Copyright (c) 2013 tokoro. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CompareObjcMatcherTests : XCTestCase
+
+@end
+
+@implementation CompareObjcMatcherTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
